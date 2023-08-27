a2ffb7f1e1de4b26c6a988c058ae7375b63551b5
Gabriel de Souza Martins <83505259+gabrielglp@users.noreply.github.com>
2023-08-27 19:48:39 -0300

    feat

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <TesteTestado/>
+  </div>
+</template>
+
+<script>
+import TesteTestado from '@/components/TesteTestado.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TesteTestado
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TesteTestado.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        <h1>teste</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'TesteTastado'
+}
+</script>
+<style lang="">
+    
+</style>
